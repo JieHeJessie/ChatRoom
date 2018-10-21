58fdcd2ff41cc35b3f1ee7be4f4afc1346fe4407
JieHe <jiehe1114@gmail.com>
2018-10-21 17:45:28 +1100

    struct initial login and register page and implement logo component

---
+++ chat-room/src/component/logo/logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LogoImage from './job-logo.jpg'
+
+import './logo.css'
+
+class Logo extends React.Component{
+    render(){
+        return(
+            <div className="logo-img">
+                <img src={LogoImage} alt="logo image"/>
+            </div>
+        )
+    }
+}
+
+export default Logo
